d0bd148bbed26005c1b50c910c114b21ea985eb5
Brett <brettpenwarden@gmail.com>
2026-03-15 10:49:07 +1300

    feat: add dummy test for /health endpoint

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from app.api.routers import health
+from starlette import status
 
 app = FastAPI(
     title="My API",
     version="0.1.0",
     description="My API description",
 )
-
-@app.get("/")
+@app.get("/", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 async def root():
     return RedirectResponse(url="/docs")
 
